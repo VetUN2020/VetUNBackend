ec7381a58aef9b6845e4746d581d03bc71460e32
felipe1297 <48300568+felipe1297@users.noreply.github.com>
2020-10-04 23:10:36 -0500

    Registro mascota

--- src/main/java/com/vetun/apirest/dao/MascotaDAO.java
+++ src/main/java/com/vetun/apirest/dao/MascotaDAO.java
@@ -2,6 +2,16 @@ package com.vetun.apirest.dao;
 
 import com.vetun.apirest.entity.Mascota;
 
+import java.util.List;
+
 public interface MascotaDAO {
+
     public void save(Mascota mascota);
+
+    public List<Mascota> findAll();
+
+    public Mascota findById(int id);
+
+    //Busca mascota segun el id del dueño y el nombre de la mascota
+    public Mascota findByName(String name, int id);
 }
--- src/main/java/com/vetun/apirest/service/MascotaService.java
+++ src/main/java/com/vetun/apirest/service/MascotaService.java
@@ -1,7 +1,18 @@
 package com.vetun.apirest.service;
 
 import com.vetun.apirest.entity.Mascota;
+import java.util.List;
 
 public interface MascotaService {
+
     public void save(Mascota mascota);
+
+    public List<Mascota> findAll();
+
+    public Mascota findById(int id);
+
+    //Busca mascota segun el id del dueño y el nombre de la mascota
+    public Mascota findByName(String name, int id);
+
+
 }
